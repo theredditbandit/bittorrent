ec6eb40a71dcfe162625f7fdd8470e7736f1ab35
aryan <85390033+theredditbandit@users.noreply.github.com>
2024-01-09 20:04:32 +0530

    parse torrent file

--- cmd/mybittorrent/main.go
+++ cmd/mybittorrent/main.go
@@ -19,6 +19,21 @@ func main() {
 		}
 		jsonOutput, _ := json.Marshal(decoded)
 		fmt.Println(string(jsonOutput))
+	} else if command == "info" {
+		fileName := os.Args[2]
+		fileReader, err := os.Open(fileName)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
+		content, err := bencode.Decode(fileReader)
+		if err != nil {
+			fmt.Printf("err: %v\n", err)
+		}
+		metaInfo, _ := content.(map[string]interface{})
+        fmt.Printf("Tracker URL: %v\n",metaInfo["announce"])
+        info , _ := metaInfo["info"].(map[string]interface{})
+        fmt.Printf("Length: %v\n",info["length"])
+
 	} else {
 		fmt.Println("Unknown command: " + command)
 		os.Exit(1)
